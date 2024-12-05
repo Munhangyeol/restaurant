5dddb1afef8c62fc2cbe600e45032f07c7085479
Munhangyeol <mhg10181018@gmail.com>
2024-12-05 15:29:25 +0900

    [V1] feat: PastaPart 구현 완료

---
+++ v1/src/main/java/restaurant/restaurant/domain/Pasta/OilPastaPart.java
@@ -0,0 +1,37 @@
+package restaurant.restaurant.domain.Pasta;
+
+import java.util.Arrays;
+
+public class OilPastaPart extends PastaPart{
+    String []ingredients;
+    String type;
+    public OilPastaPart(String type){
+        this.type=type;
+    }
+
+    public void preCook(){
+        Arrays.stream(ingredients).forEach(
+                ingredient -> System.out.println(ingredient + " ")
+        );
+        System.out.print("재료와 ");
+        Arrays.stream(untensils).forEach(
+                untensil -> System.out.println(untensil + " ")
+        );
+        System.out.println("요리기구를 준비중입니다.");
+    }
+    public void cook(){
+        Arrays.stream(ingredients).forEach(
+                ingredient -> System.out.println(ingredient + " ")
+        );
+        System.out.print("재료와 ");
+        Arrays.stream(untensils).forEach(
+                untensil -> System.out.println(untensil + " ")
+        );
+        System.out.println("요리기구를 이용해서 ");
+        System.out.print(this.type+" 요리를 요리 중입니다.");
+    }
+    public void postCook(){
+        System.out.print(this.type+" 요리가 완료 되었습니다! 맛있게 드세요!");
+    }
+
+}
