9d0c8e0c17cce0929e94bdde764c8f725868a281
Munhangyeol <mhg10181018@gmail.com>
2024-12-05 20:56:23 +0900

    [V1] Refactor: UI파트 코드 리팩토링(메서드 이름,위임등)

--- v1/src/main/java/restaurant/restaurant/domain/ui/UI.java
+++ v1/src/main/java/restaurant/restaurant/domain/ui/UI.java
@@ -22,89 +22,98 @@ public class UI {
     public UI(){
         reader = new BufferedReader(new InputStreamReader(System.in));
     }
+
+    public void run() throws IOException {
+        int menu=selectMenu();
+        int detailedMenu=selectDetailMenu(menu);
+        switch (menu){
+            case 1:
+                takeCookingSteakSteps(detailedMenu);
+                break;
+            case 2:
+                takeCookingPastaStpes(detailedMenu);
+                break;
+            case 3:
+                takeCookingPizzaSteps(detailedMenu);
+                break;
+        }
+    }
     public int selectMenu() throws IOException {
-        System.out.println("안녕하세요! 어떤 파트의 음식을 드시고 싶으세요?");
-        System.out.println("1: Steak요리 || 2: Pasta요리 || 3: Pizza요리");
+        printSelectMenu("안녕하세요! 어떤 파트의 음식을 드시고 싶으세요?", "1: Steak요리 || 2: Pasta요리 || 3: Pizza요리");
         return Integer.parseInt(reader.readLine());
     }
     public int selectDetailMenu(int mainMenu) throws IOException {
         int detailMenu = 0;
         switch (mainMenu){
-        case 1:{
-            System.out.println("Steak 중 어떤 Steak 메뉴를 고르시겠어요?");
-            System.out.println("1: T-Born Steak || 2: Sir-loin Steak || 3: Rib-eye Steak");
-            detailMenu=Integer.parseInt(reader.readLine());
-            break;
+            case 1:{
+                printSelectMenu("Steak 중 어떤 Steak 메뉴를 고르시겠어요?", "1: T-Born Steak || 2: Sir-loin Steak || 3: Rib-eye Steak");
+                detailMenu=Integer.parseInt(reader.readLine());
+                break;
             }
-        case 2:
-            System.out.println("Pasta 중 어떤 Pasta 메뉴를 고르시겠어요?");
-            System.out.println("1: Tomato-Pasta || 2: Cream-Pasta || 3: Oil-Pasta");
-            detailMenu=Integer.parseInt(reader.readLine());
-            break;
-        case 3:
-            System.out.println("Pizza 중 어떤 Pizza 메뉴를 고르시겠어요?");
-            System.out.println("1: Peperoni-Pizza || 2: Cheeze-Pizza || 3: Gorgonzola-Pizza");
-            detailMenu=Integer.parseInt(reader.readLine());
-            break;
+            case 2:
+                printSelectMenu("Pasta 중 어떤 Pasta 메뉴를 고르시겠어요?", "1: Tomato-Pasta || 2: Cream-Pasta || 3: Oil-Pasta");
+                detailMenu=Integer.parseInt(reader.readLine());
+                break;
+            case 3:
+                printSelectMenu("Pizza 중 어떤 Pizza 메뉴를 고르시겠어요?", "1: Peperoni-Pizza || 2: Cheeze-Pizza || 3: Gorgonzola-Pizza");
+                detailMenu=Integer.parseInt(reader.readLine());
+                break;
         }
         return detailMenu;
     }
-    public void run() throws IOException {
-        int menu=selectMenu();
-        int detailedMenu=selectDetailMenu(menu);
-        if(menu==1){
-            SteakPart steakPart = null;
-            switch (detailedMenu){
-                case 1:
-                    steakPart = new TBornSteakPart("T-Born",new String[]{"T-Born meat","Butter"});
-                    break;
-                case 2:
-                    steakPart = new SirloinSteakPart("Sir-loin", new String[]{"Sir-loin meat", "Butter"});
-                    break;
-                case 3:
-                    steakPart = new RibeyeSteak("Rib-eye",new String[]{"Rib-eye meat","Butter"});
-                    break;
-            }
-            System.out.println("!!");
-            steakPart.preCook();
-            steakPart.cook();
-            steakPart.postCook();
-        }
-        if(menu==2){
-            PastaPart pastaPart = null;
-            switch (detailedMenu){
-                case 1:
-                    pastaPart = new TomatoPastaPart("Tomato",new String[]{"Tomato","Noodle"});
-                    break;
-                case 2:
-                    pastaPart = new CreamPastaPart("Cream", new String[]{"Cream", "Noodle"});
-                    break;
-                case 3:
-                    pastaPart = new OilPastaPart("Oil",new String[]{"Oil","Noodle"});
-                    break;
-            }
-            pastaPart.preCook();
-            pastaPart.cook();
-            pastaPart.postCook();
-        }
-        if(menu==3){
-            PizzaPart pizzaPart = null;
-            switch (detailedMenu){
-                case 1:
-                    pizzaPart = new PeperoniPizzaPart("Peperoni",new String[]{"Peperoni","Bread"});
-                    break;
-                case 2:
-                    pizzaPart = new CheezePizzaPart("Cheeze", new String[]{"Cheeze", "Bread"});
-                    break;
-                case 3:
-                    pizzaPart = new GorgonzolaPizzaPart("Gorgonzola",new String[]{"Gorgonzola","Bread"});
-            }
-            pizzaPart.preCook();
-            pizzaPart.cook();
-            pizzaPart.postCook();
 
-        }
-
+    private void printSelectMenu(String notice, String selectedMenu) {
+        System.out.println(notice);
+        System.out.println(selectedMenu);
     }
-
+    private  void takeCookingSteakSteps(int detailedMenu) {
+        SteakPart steakPart = null;
+        switch (detailedMenu){
+            case 1:
+                steakPart = new TBornSteakPart("T-Born",new String[]{"T-Born meat","Butter"});
+                break;
+            case 2:
+                steakPart = new SirloinSteakPart("Sir-loin", new String[]{"Sir-loin meat", "Butter"});
+                break;
+            case 3:
+                steakPart = new RibeyeSteak("Rib-eye",new String[]{"Rib-eye meat","Butter"});
+                break;
+        }
+        steakPart.preCook();
+        steakPart.cook();
+        steakPart.postCook();
+    }
+    private void takeCookingPastaStpes(int detailedMenu) {
+        PastaPart pastaPart = null;
+        switch (detailedMenu){
+            case 1:
+                pastaPart = new TomatoPastaPart("Tomato",new String[]{"Tomato","Noodle"});
+                break;
+            case 2:
+                pastaPart = new CreamPastaPart("Cream", new String[]{"Cream", "Noodle"});
+                break;
+            case 3:
+                pastaPart = new OilPastaPart("Oil",new String[]{"Oil","Noodle"});
+                break;
+        }
+        pastaPart.preCook();
+        pastaPart.cook();
+        pastaPart.postCook();
+    }
+    private void takeCookingPizzaSteps(int detailedMenu) {
+        PizzaPart pizzaPart = null;
+        switch (detailedMenu){
+            case 1:
+                pizzaPart = new PeperoniPizzaPart("Peperoni",new String[]{"Peperoni","Bread"});
+                break;
+            case 2:
+                pizzaPart = new CheezePizzaPart("Cheeze", new String[]{"Cheeze", "Bread"});
+                break;
+            case 3:
+                pizzaPart = new GorgonzolaPizzaPart("Gorgonzola",new String[]{"Gorgonzola","Bread"});
+        }
+        pizzaPart.preCook();
+        pizzaPart.cook();
+        pizzaPart.postCook();
+    }
 }
